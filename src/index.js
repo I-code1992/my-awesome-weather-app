bfafc9a07d8fd970911b7d5481ba1ef14a074392
I-code1992 <138527013+I-code1992@users.noreply.github.com>
2023-07-09 17:14:58 +0200

    link folders in index

--- src/index.js
+++ src/index.js
@@ -58,5 +58,5 @@ let city = "london";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(showTemperature);
-}
+
 axios.get(apiUrl).then(getLocation);
